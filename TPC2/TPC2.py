b21bd095d2dbd76ac8963346912d4125f43ce195
josemoreira15 <73501029+josemoreira15@users.noreply.github.com>
2023-02-23 01:20:20 +0000

    Update TPC2.py

--- TPC2/TPC2.py
+++ TPC2/TPC2.py
@@ -25,6 +25,7 @@ def process(list,flag,sum):
             if it < len(list)-1 and (list[it] == 'O' or list[it] == 'o') and (list[it+1] == 'N' or list[it+1] == 'n'):
                 flag = 1
                 it += 1
+                print('>>> The counter is ON! <<<')
             elif list[it] == '=':
                 print(f'[>>> The sum is {sum}! <<<]')
                 it += 1
@@ -34,6 +35,7 @@ def process(list,flag,sum):
             if it < len(list)-2 and (list[it] == 'O' or list[it] == 'o') and (list[it+1] == 'F' or list[it+1] == 'f') and (list[it+2] == 'F' or list[it+2] == 'f'):
                 flag = 0
                 it += 3
+                print('>>> The counter is OFF! <<<')
             elif list[it].isdigit():
                 result = get_number(list[it:])
                 it += result[0]
@@ -49,13 +51,11 @@ def process(list,flag,sum):
 
 def main():
     sum, flag = 0,1
+    print('>>> The counter is ON! <<<')
     while(1):
         s = input('>>> Type your string: ')
 
         sum,flag = process(s,flag,sum)
-        print(f'Sum: {sum}')
-        print(f'Flag: {flag}')
-
 
 if __name__ == "__main__":
     main()
